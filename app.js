47e88cefe7fa07754edf8911b647e32c5c0b40ff
Vikas Gola <vgola261@gmail.com>
2025-03-18 20:59:31 +0530

    Update app.js
    chang2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgcontainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turn0 = true; // Player starts first
+let turn0 = true;
 
 const winpattern = [
   [0, 1, 2],
@@ -24,32 +24,19 @@ const resetGame = () => {
   resetbtn.classList.remove("hide"); // Unhide reset button on new game
 };
 
-// Player Move
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turn0) {
-      box.innerText = "O";
-      box.disabled = true;
+      box.innerText = "o";
       turn0 = false;
-      checkwinner();
-      if (!turn0) {
-        setTimeout(computerMove, 500); // Small delay to mimic real gameplay
-      }
+    } else {
+      box.innerText = "x";
+      turn0 = true;
     }
+    box.disabled = true;
+    checkwinner();
   });
 });
-
-// Computer Move - Selects a random available box
-const computerMove = () => {
-  let availableBoxes = Array.from(boxes).filter((box) => box.innerText === "");
-  if (availableBoxes.length > 0) {
-    let randomBox = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
-    randomBox.innerText = "X";
-    randomBox.disabled = true;
-    turn0 = true;
-    checkwinner();
-  }
-};
 
 const disableBoxes = () => {
   for (let box of boxes) {
@@ -77,17 +64,12 @@ const checkwinner = () => {
     let pos2val = boxes[pattern[1]].innerText;
     let pos3val = boxes[pattern[2]].innerText;
 
-    if (pos1val !== "" && pos1val === pos2val && pos2val === pos3val) {
-      showWinner(pos1val);
-      return;
+    if (pos1val !== "" && pos2val !== "" && pos3val !== "") {  
+      if (pos1val === pos2val && pos2val === pos3val) {  
+        showWinner(pos1val);  
+        return;  
+      }  
     }
-  }
-
-  // Check for Draw
-  if ([...boxes].every((box) => box.innerText !== "")) {
-    msg.innerText = "It's a Draw!";
-    msgcontainer.classList.remove("hide");
-    disableBoxes();
   }
 };
 
